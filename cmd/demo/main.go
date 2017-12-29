01aaf958df8c51bbb7b7ec252e7918b4aef1fe72
StabbyCutyou <smattkelly@gmail.com>
2017-12-28 22:44:15 -0500

    Renaming mux to pub, cus it isn't actually a mux

--- cmd/demo/main.go
+++ cmd/demo/main.go
@@ -8,30 +8,27 @@ import (
 )
 
 func main() {
-	m := spub.New(make(chan error), time.Millisecond*1)
+	p := spub.New(time.Millisecond * 1)
 	wg := sync.WaitGroup{}
 	wg.Add(2)
 	go func() {
-		for range m.Err() {
+		for range p.Err() {
 		}
 	}()
-
 	for i := 0; i < 2; i++ {
 		l := spub.Listener{
 			C: make(chan []byte),
 		}
-		m.Register(l)
+		p.Register(l)
 		go func(il *spub.Listener) {
-
 			for range il.C {
 			}
 			wg.Done()
 		}(&l) // pop off endlessly
 	}
-
 	for i := 0; i < 100000; i++ {
-		m.Send(make([]byte, 0))
+		p.Send(make([]byte, 0))
 	}
-	m.Close()
+	p.Close()
 	wg.Wait()
 }
